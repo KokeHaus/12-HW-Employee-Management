a8650ac79cc18af0a8dff6a8b4a891615635830d
KokeHaus <austinbcw@gmail.com>
2022-05-03 10:46:16 -0500

    Added DB

--- schema.sql
+++ schema.sql
@@ -1,18 +1,27 @@
-DROP DATABASE IF EXISTS movies_db;
-CREATE DATABASE movies_db;
 
-USE movies_db;
+CREATE database employee_db;
 
-CREATE TABLE movies (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  movie_name VARCHAR(100) NOT NULL
+USE employee_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name TEXT NULL,
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE reviews (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    movie_id INT,
-    review TEXT NOT NULL,
-    FOREIGN KEY (movie_id)
-    REFERENCES movies(id)
-    ON DELETE SET NULL
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title TEXT NULL,
+  salary INT NULL,
+  department_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+   id INT NOT NULL AUTO_INCREMENT,
+  first_name TEXT NULL,
+  last_name TEXT NULL,
+  role_id INT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY (id)
 );
--- seeds.sql
+++ seeds.sql
@@ -1,15 +1,30 @@
-INSERT INTO movies (movie_name)
-VALUES ("Lion King"),
-       ("The Godfather"),
-       ("West Side Story"),
-       ("Parasite"),
-       ("The Wizard of Oz");
+USE employee_trackerDB;
 
-INSERT INTO reviews (movie_id, review)
-VALUES (1, "Zazu is underrated. Give that hornbill a sequel!"),
-       (2, "I'm gonna make him an offer you can't refuse, watch this movie"),
-       (1, "Scar is the lion everyone loves to hate"),
-       (3, "Ten years of ballet and three years of tap to join a gang in this neighborhood"),
-       (5, "The tin man gave a metallic, hollow performance"),
-       (1, "Hakuna matata"),
-       (5, "Those flying monkeys are nightmare fuel!");
+INSERT INTO department (name)
+VALUES 
+("Sales"), 
+("Engineering"), 
+("Finance"), 
+("Legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES 
+("Sales Lead", 100000, 1), 
+("Salesperson", 80000, 1), 
+("Lead Engineer", 150000, 2), 
+("Software Engineer", 120000, 2), 
+("Account Manager", 160000, 3),
+("Accountant", 125000, 3), 
+("Legal Team Lead", 250000, 4), 
+("Lawyer", 190000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES 
+("John", "Doe", 1), 
+("Mike", "Chan", 2), 
+("Ashley", "Rodriguez", 3),
+("Kevin", "Tupik", 4), 
+("Kunal", "Singh", 5), 
+("Malia", "Brown", 6), 
+("Sarah", "Lourd", 7), 
+("Tom", "Allen", 5);
